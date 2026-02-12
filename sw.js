c37172ebfe7a14324ce79610d3dfe0874df9b5b7
evertonjoipai48-oss <evertonjoipai48@gmail.com>
2026-02-12 11:02:34 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,1 +1,21 @@
 
+self.addEventListener("install", e => {
+  e.waitUntil(
+    caches.open("solar-app").then(cache =>
+      cache.addAll([
+        "./",
+        "./index.html",
+        "./style.css",
+        "./app.js",
+        "./manifest.json",
+        "./favicon.ico"
+      ])
+    )
+  );
+});
+
+self.addEventListener("fetch", e => {
+  e.respondWith(
+    caches.match(e.request).then(r => r || fetch(e.request))
+  );
+});
